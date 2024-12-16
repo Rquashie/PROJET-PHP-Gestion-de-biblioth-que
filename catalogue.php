ffab4e5f5411fd6b490bcd65bd4d8c3fbd87a27f
rQuashie <kangoojuniors93@gmail.com>
2024-12-16 22:21:42 +0100

    feat(pageUser.php) : Ajouter de nouveaux onglets de navigations qui vont permettre l'affichage des livres et la réservation feat(catalogue.php) : Affichage des livres présent dans la bibliothèque

---
+++ catalogue.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+$bdd = new PDO('mysql:host=localhost;port=3306;dbname=rqe_librairie', 'root', '');
+if(isset($_SESSION["login"])&& $_SESSION["mdp"]) {
+    echo "<p> Cataloge des livres disponibles</p>" ;
+    $sql = $bdd -> prepare("SELECT * FROM livre");
+    $sql -> execute();
+    $i = 1 ;
+    while($livre = $sql -> fetch()) {
+
+        echo "<form>";
+        echo "<table border=1>";
+        echo "<tr>";
+        echo "<th> titre </th>";
+        echo "<th> Année </th>";
+        echo "<th> Résumé </th>";
+        echo "</tr>";
+
+        echo "<tr>";
+        echo "<td>" . $livre['titre'] . "</td>";
+        echo "<td>" . $livre['annee'] . "</td>";
+        echo "<td>" . $livre['resume'] . "</td>";
+        echo "</tr>";
+        echo "</table>";
+        echo "</form>";
+        echo "<style>";
+        echo "table {border-collapse: collapse; width: 80%;}";
+        echo "th, td {border: 1px solid black; padding: 8px; text-align: left;}";
+        echo "</style>";
+    }
+}
+
+echo "<a href = 'pageUser.php'>Retour</a>";
--- pageUser.php
+++ pageUser.php
@@ -1,12 +1,22 @@
 <?php
 session_start();
+$bdd = new PDO('mysql:host=localhost;port=3306;dbname=rqe_librairie', 'root', '');
 echo "<html>" ;
 echo "<head>" ;
 echo "</head>" ;
 echo "<body>" ;
 echo "<h1> Espace utilisateur</h1>" ;
+
 if(isset($_SESSION["login"])&& $_SESSION["mdp"]) {
     echo "<h3> Bienvenue " . $_SESSION['login'] . "</h3>";
 }
+echo  "<ul>" ;
+echo "<li> <a href= 'catalogue.php'> Catalogue des livres </a> </li>" ;
+echo "<li> <a href= ''> Emprunter des livres </a> </li>" ;
+echo "</ul>" ;
+
+echo "<a href = 'deconnecter.php'> Deconnexion </a> " ;
+
+
 
 ?>
