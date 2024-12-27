e13235a73d18abf7700f737cfdaa1b44ac6eae23
rQuashie <kangoojuniors93@gmail.com>
2024-12-27 16:25:15 +0100

    feat(listeAuteur.php) : Ajout d'une barre de rechercher pour les auteurs permettant de filtrer les auteurs par nom ou par prenom

--- listeAuteur.php
+++ listeAuteur.php
@@ -1,59 +1,37 @@
 <?php
 session_start();
-//Afficher les auteurs par pays
+
 if(isset($_SESSION["login"])) {
     $bdd = new PDO('mysql:host=localhost;port=3306;dbname=rqe_librairie', 'root', '');
+    echo "<h1>Liste des auteurs</h1>";
+    $recherche = "";
+
+    echo "<table>";
+
+    echo "<form method='POST' action='listeAuteur.php'>";
+    echo "<tr>";
+    echo "<td><input type='search' name='recherche' placeholder='Recherche un auteur...' /></td>";
+    echo "<td><input type='submit' value='Valider' /></td>";
+    echo "<td><input type='reset' value='Annuler' /></td>";
+    echo "</tr>";
+    echo "</form>";
+    echo "</table>";
 
 
-    echo "<div class ='liste-auteurs'> ";
-    $sqlListeAuteur = $bdd->prepare("SELECT * from auteur ");
-    $sqlListeAuteur->execute();
-    $ligneAuteur = $sqlListeAuteur->fetch();
+    $sqlAuteur = $bdd->query('SELECT concat(prenom+nom) FROM auteur ORDER BY id_auteur DESC');
+    if (isset($_POST['recherche'])) {
+        $recherche = htmlspecialchars($_POST['recherche']);
 
+        $sqlAuteur = $bdd->query('SELECT * FROM auteur WHERE nom LIKE "%' . $recherche . '%" 
+                  or prenom like "%' . $recherche . '%" ORDER BY id_auteur DESC');
+        $sqlAuteur->execute();
+        while ($ligne = $sqlAuteur->fetch()) {
+            echo "<ul>";
+            echo "<li>" . $ligne['prenom'] . " " . $ligne['nom'] . "</li>";
+            echo "</ul>";
+        }
+    }
 
-
-    $i = 0;
-
-
-
-        echo "<div class=listAuteur>";
-        echo"<table border =1px solid>";
-
-        echo "<tr>";
-        echo "<th> Nom </th>";
-        echo "<th>Prenom</th>";
-        echo "<th>Date de naissance</th>";
-        echo "<th>Nationalité</th>";
-        echo"</tr>";
-
-       while ($ligneAuteur = $sqlListeAuteur->fetch()) {
-           $i++;
-           $ref_pays = $ligneAuteur['ref_pays'];
-           $var =[$ref_pays];
-           $nom = $ligneAuteur["nom"];
-           $prenom = $ligneAuteur["prenom"];
-           $date_naissance = $ligneAuteur["date_naissance"];
-           $sqlNationalite = $bdd->prepare("SELECT p.nom FROM pays p inner join auteur a 
-                                       on p.id_pays = a.ref_pays where a.ref_pays = ?");
-           $sqlNationalite->execute($var);
-           $data = $sqlNationalite->fetchAll();
-           $nationalite = $data[0]["nom"];
-           // Chaque ligne contient les informations d'un livre
-           echo "<tr>";
-           echo "<td>$nom</td>";
-           echo "<td>$prenom</td>";
-           echo "<td>$date_naissance</td>";
-           echo "<td>$nationalite</td>";
-       }
-           echo "</table>";
-           echo "</div>";
-
-        echo "<style>";
-        echo "table {border-collapse: collapse; width: 80%;table-layout :fixed}";
-        echo "th, td {border: 1px solid black; padding: 8px; text-align: left;}";
-        echo "</style>";
-
-
+    echo "<a href = 'pageAdmin.php'>Retour au menu principal</a>";
 }
-echo"<a href = 'pageAdmin.php'>Retour au menu principal</a>";
 ?>
