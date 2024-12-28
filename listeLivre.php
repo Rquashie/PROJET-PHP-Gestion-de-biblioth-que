725f511bda67368adecd5b287c2878a2fbfa4f11
rQuashie <kangoojuniors93@gmail.com>
2024-12-28 12:07:54 +0100

    feat(llisteLivre.php) : Ajout de la page listeLivre avec affichage et tri des livres

--- listeLivre.php
+++ listeLivre.php
@@ -5,34 +5,38 @@ class Livres{
     public function triLivres($colonne, $ordre)
     {
         global $bdd;
-        $sql = $bdd->prepare("SELECT * FROM livre order by $colonne $ordre");
-        $sql->execute();
-        while ($ligne = $sql->fetch()) {
-
-            $id_livre = $ligne['id_livre'];
-            $nom = $ligne["titre"];
-            $annee = $ligne["annee"];
-            $sqlLivreAuteur = $bdd->prepare("SELECT p.nom FROM pays p inner join auteur a 
-                                       on p.id_pays = a.ref_pays where a.ref_pays = ?");
-            $sqlNationalite->execute($var);
-            $data = $sqlNationalite->fetchAll();
-            $nationalite = $data[0]["nom"];
+        $sqlLivreAuteur = $bdd->prepare("select titre as 'Titre du livre', annee as 'Année de parution',
+            concat(a.nom,' ', a.prenom) as 'Auteur' , resume as 'Résumé'
+            from ecrire e
+            inner JOIN auteur a on a.id_auteur = e.ref_auteur
+            inner join livre l on e.ref_livre = l.id_livre
+            order by $colonne $ordre
+            ");
+        $sqlLivreAuteur->execute();
+        while ($ligne = $sqlLivreAuteur->fetch()) {
+            $titre = $ligne[0];
+            $annee = $ligne[1];
+            $auteur = $ligne[2];
+            $resume = $ligne[3];
 
             // Chaque ligne contient les informations d'un livre
             echo "<tr>";
-            echo "<td>$nom</td>";
-            echo "<td>$prenom</td>";
-            echo "<td>$date_naissance</td>";
-            echo "<td>$nationalite</td>";
+            echo "<td>$titre </td>";
+            echo "<td>$annee</td>";
+            echo "<td>$auteur </td>";
+            echo "<td>$resume</td>";
             echo "</tr>";
         }
     }
 }
-$livre = new Livres() ;
+//---------------------------PROGRAMME PRINCIPAL------------------------------
+$livres = new Livres() ;
+
+
 
 if(isset($_SESSION["login"])) {
     $bdd = new PDO('mysql:host=localhost;port=3306;dbname=rqe_librairie', 'root', '');
-    echo "<h2>Liste des livres</h2>";
+    echo "<h2>Liste des livres </h2>";
     $recherche = "";
 
     echo "<div class ='contenu'>";
@@ -45,97 +49,85 @@ if(isset($_SESSION["login"])) {
     echo "</form>";
 
 
-    $sqlAuteur = $bdd->query('SELECT concat(prenom+nom) FROM auteur ORDER BY id_auteur DESC');
+    $sqlAuteur = $bdd->query('SELECT concat(titre) FROM livre ORDER BY id_livre DESC');
     if (isset($_POST['recherche'])) {
         $recherche = htmlspecialchars($_POST['recherche']);
 
-        $sqlAuteur = $bdd->query('SELECT * FROM auteur WHERE nom LIKE "%' . $recherche . '%" 
-                  or prenom like "%' . $recherche . '%" ORDER BY id_auteur DESC');
-        $sqlAuteur->execute();
-        while ($ligne = $sqlAuteur->fetch()) {
+        $sqlLivre = $bdd->query('SELECT * FROM livre WHERE titre LIKE "%' . $recherche . '%" 
+                 ORDER BY id_livre DESC');
+        $sqlLivre->execute();
+        while ($ligne = $sqlLivre->fetch()) {
             echo "<ul>";
-            echo "<li>" . $ligne['prenom'] . " " . $ligne['nom'] . "</li>";
+            echo "<li>" . $ligne['titre'] . "</li>";
             echo "</ul>";
         }
     }
-    echo "</div>";  //Ferme le div recherche
+    echo "</div>";
+    //------------------------Ferme le div recherche--------------------
 
-    echo "<div class='auteur'>";
-    echo "<form action='listeAuteur.php' method='POST'>";
-    echo "<select name='filtre-auteur'>";
+    echo "<div class='livre'>";
+    echo "<form action='listeLivre.php' method='POST'>";
+    echo "<select name='filtre-livre'>";
     echo "<option value='affiche-tout' selected='selected'>Afficher tout</option>";
-    echo "<option value='Trie-par-nom-asc'>Nom croissant</option>";
-    echo "<option value='Trie-par-nom-desc'>Nom deroissant</option>";
-    echo "<option value='Trie-par-prenom-asc'>Prenom croissant</option>";
-    echo "<option value='Trie-par-prenom-desc'>Prenom decroissant</option>";
-    echo "<option value='date-asc'>Date de naissance croissant</option>";
-    echo "<option value='date-desc'>Date de naissance decroissant</option>";
+    echo "<option value='Trie-par-titre-asc'>Titre ordre croissant A-Z </option>";
+    echo "<option value='Trie-par-titre-desc'>Titre ordre decroissant Z-A</option>";
+    echo "<option value='Trie-par-annee-asc'>Année de parution par ordre croissant</option>";
+    echo "<option value='Trie-par-annee-desc'>Année de parution par ordre decroissant</option>";
     echo "</select>";
     echo "<input name='Valider' type='submit' value='filtre'/>";
     echo "</form>";
 
     //Colonnes
-    echo "<form action = 'listeAuteur.php' method='POST'> ";
-    echo "<table border='1'>";
-    echo "<th>Nom </th>";
-    echo "<th>prenom </th>";
-    echo "<th>Date de naissance</th>";
-    echo "<th>Nationalité </th>";
+    echo "<form action = 'listeLivre.php' method='POST'> ";
+    echo "<table border='4'>";
+    echo "<th>Titre </th>";
+    echo "<th>Année parution</th>";
+    echo "<th>Auteur</th>";
+    echo "<th>Résumé</th>";
     echo "<tr>";
 
 
-    $filtreAuteur = "";
-    if (!empty($_POST['filtre-auteur'])) {
-        $filtreAuteur = $_POST['filtre-auteur'];
-        if ($filtreAuteur == "Trie-par-nom-asc") {
-            $auteur->triAuteurs('nom', 'ASC');
+    $filtreLivre = "";
+    if (!empty($_POST['filtre-livre'])) {
+        $filtreLivre = $_POST['filtre-livre'];
+        if ($filtreLivre == "Trie-par-titre-asc") {
+            $livres->triLivres('titre', 'ASC');
         }
-        if ($filtreAuteur == "Trie-par-nom-desc") {
-            $auteur->triAuteurs('nom', 'DESC');
+        if ($filtreLivre == "Trie-par-titre-desc") {
+            $livres->triLivres('titre', 'DESC');
         }
-        if ($filtreAuteur == "Trie-par-prenom-asc") {
-            $auteur->triAuteurs('prenom', 'ASC');
+        if ($filtreLivre == "Trie-par-annee-asc") {
+            $livres->triLivres('annee', 'ASC');
         }
-        if ($filtreAuteur == "Trie-par-prenom-desc") {
-            $auteur->triAuteurs('prenom', 'DESC');
+        if ($filtreLivre == "Trie-par-annee-desc") {
+            $livres->triLivres('annee', 'DESC');
         }
-        if ($filtreAuteur == "date-asc") {
-            $auteur->triAuteurs('date_naissance', 'ASC');
-        }
-        if ($filtreAuteur == "date-desc") {
-            $auteur->triAuteurs('date_naissance', 'DESC');
-        }
+    } else {
+        $livres->triLivres('id_auteur', 'ASC');
     }
-    else {
-        $auteur -> triAuteurs('id_auteur', 'ASC');
-    }
+
 
     echo "</table>";
     echo "</form>";
-} else {
+    echo "</div>";
+    echo "<br>";
+    echo "<br>";
+
+
+    echo "<a href = 'pageAdmin.php'>Retour au menu principal</a>";
+    echo "</div>";
+
+
+    echo "<style>";
+
+    echo "body{background-color: #f2f2f2 ;}";
+    echo".auteur{background-color:light red;}";
+    echo".recherche td{padding:20px;}";
+    echo ".bas-page{display:fle}";
+    echo "table {border-collapse: collapse; width: 80%;table-layout : fixed ;}";
+    echo "th, td {border: 1px solid black; padding: 8px; text-align: left;}";
+    echo "</style>";
 
 }
-echo "</table>";
-echo "</form>";
-
-
-echo "<br>";
-echo "<br>";
-
-echo "<a href = 'pageAdmin.php'>Retour au menu principal</a>";
-echo "</div>";
-
-
-echo "<style>";
-
-echo "body{background-color: #f2f2f2 ;}";
-echo ".contenu{display: flex;}";
-echo".auteur{background-color:light red;}";
-echo".recherche td{padding:20px;}";
-echo ".bas-page{display:fle}";
-echo "table {border-collapse: collapse; width: 80%;table-layout : fixed ;}";
-echo "th, td {border: 1px solid black; padding: 8px; text-align: left;}";
-echo "</style>";
-
 ?>
 
